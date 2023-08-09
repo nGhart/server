ad599a95c7aa59e6a4abf4b1e5bfdae355d942f5
nGhart <naaghart@gmail.com>
2023-08-09 16:14:18 +0000

    index.js added

---
+++ index.js
@@ -0,0 +1,4 @@
+const http = require('http');
+
+const server = http.createServer();
+server.listen(3000, '127.0.0.1'); //3000 is the port, second numbers refers to the local host and can be replaced local host
